9e5b5f34cbe8d6113403cbe12a85ae662b42aa34
Joaquin_FG <48914838+JoaquinFG@users.noreply.github.com>
2020-08-17 19:47:45 +0100

    loading styles

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,21 +1,23 @@
 import React from 'react'
+import confLogo from '../images/platziconf-logo.svg'
+import './styles/Badge.css'
 
 class Badge extends React.Component{
     render(){
         return(
-            <div>
-                <div>
-                    <img src="" alt="Logo"/>
+            <div className="Badge">
+                <div className="Badge__header">
+                    <img src={confLogo} alt="LogoConf"/>
                 </div>
-                <div>
-                    <img src="" alt="Avatar" />
+                <div className="Badge__section-name">
+                    <img className="Badge__avater" src="https://s.gravatar.com/avatar/ab6d453f438e2e4fb6ded179639adc47?s=80" alt="Avatar" />
                     <h1>Joaquin <br/> Ferreira</h1>
                 </div>
-                <div>
-                    <p>Multiplatform app developer</p>
-                    <p>@Joakin_FG</p>
+                <div className="Badge__section-info">
+                    <h3>Multiplatform app developer</h3>
+                    <div>@Joakin_FG</div>
                 </div>
-                <div>
+                <div className="Badge__footer">
                     #Badge_Project
                 </div>
             </div>
