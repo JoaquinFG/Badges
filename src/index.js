4393aabf8f51990b3f5784a7c4865dde755dbd31
Joaquin_FG <48914838+JoaquinFG@users.noreply.github.com>
2020-08-18 15:22:41 +0100

    Navbar and Badge component

--- src/index.js
+++ src/index.js
@@ -3,17 +3,8 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
 
-import Badge from "./components/Badge";
+import Badgenew from "./pages/BadgeNew";
 
 const container = document.getElementById("app");
 
-ReactDOM.render(
-  <Badge
-    firstName="Joaquin"
-    lastName="Ferreira"
-    avatar="https://s.gravatar.com/avatar/ab6d453f438e2e4fb6ded179639adc47?s=80"
-    jobTitle="Multiplatform app developer"
-    twitter="Joakin_FG"
-  />,
-  container
-);
+ReactDOM.render(<Badgenew />, container);
---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import header from '../images/badge-header.svg';
+import Navbar from '../components/Navbar';
+import Badge from "../components/Badge.js";
+import './styles/BadgeNew.css';
+
+class BadgeNew extends React.Component{
+    render(){
+        return (
+          <div>
+            <Navbar />
+            <div className="BadgeNew__hero">
+              <img className="img-fluid" src={header} alt="" />
+            </div>
+            <div className="container">
+              <div className="row">
+                <div className="col">
+                  <Badge
+                    firstName="Joaquin"
+                    lastName="Ferreira"
+                    twitter="Joakin_FG"
+                    jobTitle="Multiplatform app developer"
+                    avatar="https://s.gravatar.com/avatar/ab6d453f438e2e4fb6ded179639adc47?s=80"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+    }
+}
+
+export default BadgeNew;
