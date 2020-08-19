ef52e2c6fe414ec3046f9e5ad25b93334b2ffba7
Joaquin_FG <48914838+JoaquinFG@users.noreply.github.com>
2020-08-19 17:00:29 +0100

    State changes badge

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,38 +1,69 @@
-import React from 'react'
-import header from '../images/badge-header.svg';
-import Navbar from '../components/Navbar';
+import React from "react";
+import header from "../images/badge-header.svg";
+import Navbar from "../components/Navbar";
 import Badge from "../components/Badge";
 import BadgeForm from "../components/BadgeForm";
 
-import './styles/BadgeNew.css';
+import "./styles/BadgeNew.css";
 
-class BadgeNew extends React.Component{
-    render(){
-        return (
-          <div>
-            <Navbar />
-            <div className="BadgeNew__hero">
-              <img className="img-fluid" src={header} alt="" />
+class BadgeNew extends React.Component {
+  state = {
+    form: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      jobTitle: "",
+      twitter: "",
+    },
+  };
+
+  handleChange = (e) => {
+    /* const nextForm = this.state.form
+    nextForm[e.target.name] = e.target.value; */
+
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="" />
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <Badge
+                firstName={this.state.form.firstName}
+                //"Joaquin"
+                lastName={this.state.form.lastName}
+                //"Ferreira"
+                twitter={this.state.form.twitter}
+                //"Joakin_FG"
+                jobTitle={this.state.form.jobTitle}
+                //"Multiplatform app developer"
+                email={this.state.form.email}
+                //joaquinferreirag@gmail.com
+                avatar="https://s.gravatar.com/avatar/ab6d453f438e2e4fb6ded179639adc47?s=80"
+              />
             </div>
-            <div className="container">
-              <div className="row">
-                <div className="col-6">
-                  <Badge
-                    firstName="Joaquin"
-                    lastName="Ferreira"
-                    twitter="Joakin_FG"
-                    jobTitle="Multiplatform app developer"
-                    avatar="https://s.gravatar.com/avatar/ab6d453f438e2e4fb6ded179639adc47?s=80"
-                  />
-                </div>
-                <div className="col-6">
-                  <BadgeForm/>
-                </div>
-              </div>
+            <div className="col-6">
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
             </div>
           </div>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default BadgeNew;
