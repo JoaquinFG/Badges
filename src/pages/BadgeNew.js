a851f5be247fab434e742c82aecf05c092346b13
Joaquin_FG <48914838+JoaquinFG@users.noreply.github.com>
2020-08-22 21:20:52 +0100

    Post petition states

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -5,9 +5,12 @@ import BadgeForm from "../components/BadgeForm";
 import api from '../api'
 
 import "./styles/BadgeNew.css";
+import PageLoading from "../components/PageLoading";
 
 class BadgeNew extends React.Component {
   state = {
+    loading: false,
+    error: null,
     form: {
       firstName: "",
       lastName: "",
@@ -35,12 +38,17 @@ class BadgeNew extends React.Component {
     try {
       await api.badges.create(this.state.form)
       this.setState({ loading: false});
+      this.props.history.push('/badges');
     } catch (error) {
       this.setState({loading: false, error: error})
     }
   }
 
   render() {
+    if (this.state.loading){
+      return <PageLoading/>
+    }
+
     return (
       <React.Fragment>
         <div className="BadgeNew__hero">
@@ -68,6 +76,7 @@ class BadgeNew extends React.Component {
                 onChange={this.handleChange}
                 onSubmit={this.handleSubmit}
                 formValues={this.state.form}
+                error={this.state.error}
               />
             </div>
           </div>
