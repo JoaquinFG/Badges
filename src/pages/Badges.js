c1066528b0de81e3a0acc9cd872033d43a0dbbbc
Joaquin_FG <48914838+JoaquinFG@users.noreply.github.com>
2020-08-22 17:43:29 +0100

    Reading from API

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -4,14 +4,40 @@ import { Link } from "react-router-dom";
 import "./styles/Badges.css";
 import confLogo from "../images/badge-header.svg";
 import BadgesList from "../components/BadgesList";
+import api from '../api'
 
 export default class Badges extends Component {
   
   state = {
-    data: [],
+    loading: true,
+    error: null,
+    data: undefined,
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+  
+  fetchData = async () => {
+    this.setState({ loading: true, error: null})
+
+    try {
+      const data = await api.badges.list();
+      this.setState({ loading: false, data: data})
+    } catch (error) {
+      this.setState({ loading: false, error: error });
+    }
   }
 
   render() {
+
+    if (this.state.loading === true){
+      return 'Loading...'
+    }
+    if (this.state.error) {
+      return `Error: ${this.state.error.message}` ;
+    }
+
     return (
       <React.Fragment>
         <div className="Badges">
